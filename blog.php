40d1e2ad62af52f35106358c473a17acc9afdab8
TheJadeEmperor <benjamin_louie@drugfree.org>
2019-03-19 10:36:17 -0400

    blog & PR Links

---
+++ blog.php
@@ -0,0 +1,39 @@
+<?
+$siteNUS = 'http://neobuxultimatestrategy.com/';
+$localNUS = 'http://localhost/NeobuxUltimateStrategy/';
+$prlog = 'https://biz.prlog.org/';
+?>
+
+<center>
+    <table>
+    <tr valign="top">
+        <td>
+            <div class="contentBox">
+
+<p>PRLog acct page<br />
+<a target="_BLANK" href="https://www.prlog.org/pub/manage.html">https://www.prlog.org/pub/manage.html</a>
+</p>
+
+<p>PRLog Business Proile - Goldmoney<br />
+<a target="_BLANK" href="<?=$prlog?>goldmoney/"><?=$prlog?>goldmoney</a></p>
+
+<p>PRLog Business Proile - NUS<br />
+<a target="_BLANK" href="<?=$prlog?>neobux/"><?=$prlog?>neobux</a></p>
+
+<p>NUS Blog Admin | Local<br />
+<a target="_BLANK" href="<?=$localNUS?>admin/pages/postList.php"><?=$localNUS?>admin/pages/postList.php</a></p>
+
+<p>NUS Blog Admin | Live<br />
+<a target="_BLANK" href="<?=$siteNUS?>admin/pages/postList.php"><?=$siteNUS?> admin/pages/postList.php</a> </p>
+
+<p>NUS Blog | Local<br />
+<a target="_BLANK" href="<?=$localNUS?>?action=posts"><?=$localNUS?>?action=posts</a></p>
+
+<p>NUS Blog | Live<br />
+<a target="_BLANK" href="<?=$siteNUS?>?action=posts"><?=$siteNUS?>?action=posts</a></p>
+
+		</div>
+	</td>
+	</tr>
+</table>
+</center>
